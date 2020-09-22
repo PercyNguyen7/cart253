032997d9cfa16578362ed5f9bc66247f3086bb8b
PercyNguyen7 <70938896+PercyNguyen7@users.noreply.github.com>
2020-09-22 02:46:12 -0400

    Drawing experiment: Testing squares and lines

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -9,7 +9,12 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
+createCanvas(500,500);
+background(200,200,200);
 
+point(50,50);
+rect(0,0,250,200)
+line(0,0,250,500)
 }
 
 // draw()
