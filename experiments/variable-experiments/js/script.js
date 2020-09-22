68eb50776a7933cda0fb3ae72d72b550214b6f63
PercyNguyen7 <70938896+PercyNguyen7@users.noreply.github.com>
2020-09-22 13:24:55 -0400

    Variable-experiments: Change variables

--- experiments/variable-experiments/js/script.js
+++ experiments/variable-experiments/js/script.js
@@ -4,24 +4,23 @@ Pippin Barr
 
 Here is a description of this template p5 project.
 **************************************************/
-
+let backgroundShade = (250,202,220) ;
+let circleX = 250
+let circleY = 250
+let circleSize = 200
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-createCanvas(500,500);
-background(200,200,200);
-
-point(50,50);
-rect(0,0,250,200)
-line(0,0,250,500)
+  createCanvas(500,500);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-  background(255,0,0)
-  rectMode(CENTER)
-  rect(250.250,100,100)
+  background(backgroundShade);
+  circleSize = circleSize * 1.01;
+  circleY = circleY / 1.01;
+  ellipse(circleX, circleY, circleSize)
 }
