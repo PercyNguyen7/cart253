1b48f0bfb29e6f0614045c751f8d5c4c6d091a4f
PercyNguyen7 <70938896+PercyNguyen7@users.noreply.github.com>
2020-09-22 19:50:46 -0400

    E1: Defined all variables for three shapes

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -4,11 +4,44 @@ Pippin Barr
 
 Here is a description of this template p5 project.
 **************************************************/
+let circle1 = {
+  x:0,
+  y:100,
+  size:50,
+  growthRate:1,
+  speed: 1,
+  fill:255,
+  alpha:200,
+}
+
+let rectangle1 = {
+  x:500,
+  y:250,
+  size: 75,
+  speed: -1,
+  fill: 255,
+  alpha:200
+
+let square1= {
+  x:500,
+  y:250,
+  size: 75,
+  speed: 1,
+  fill: 255,
+  alpha:200
+
+  }
+
+
+}
+
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+
+
 
 }
 
@@ -16,5 +49,6 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+
 
 }
