54202c9756e50b241a52075052651c2081b5c7e5
PercyNguyen7 <70938896+PercyNguyen7@users.noreply.github.com>
2020-09-22 21:06:01 -0400

    E1: Background now changes from black to purple, new square following mouse added

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -4,6 +4,11 @@ Pippin Barr
 
 Here is a description of this template p5 project.
 **************************************************/
+let bg = {
+  r: 0,
+  g:0,
+  b:0,
+}
 let circle1 = {
   x:0,
   y:100,
@@ -11,7 +16,7 @@ let circle1 = {
   growthRate:0.3,
   speed: 1,
   fill:255,
-  alpha:150,
+  alpha:180,
 }
 
 let rectangle1 = {
@@ -21,16 +26,15 @@ let rectangle1 = {
   height:250,
   speed: -1,
   fill: 255,
-  alpha:150
+  alpha:180
 }
 
 let square1= {
   x:500,
   y:250,
-  size: 75,
-  speed: 1,
-  fill: 255,
-  alpha:150
+  size: 30,
+  fill: 150,
+  alpha:50
 }
 
 
@@ -50,10 +54,14 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
-  background(0,mouseY,mouseX);
+
+//Background
+  background(bg.r, bg.g, bg.b);
+  bg.r = map(circle1.size,50,width/4,0,153);
+  bg.g = map(circle1.size,50,width/4,0,50);
+  bg.b = map(circle1.size,50,width/4,0,204);
 
 //Left circle
-
 circle1.x = circle1.x + circle1.speed;
 circle1.x = constrain(circle1.x,0, width/2,width);
 circle1.size = circle1.size + circle1.growthRate;
@@ -61,12 +69,17 @@ circle1.size = constrain(circle1.size,0,width/4);
 fill(circle1.fill,circle1.alpha)
 ellipse(circle1.x, circle1.y, circle1.size);
 
-//right rectangle
+//Right rectangle
 rectangle1.x= rectangle1.x + rectangle1.speed;
 rectangle1.x = constrain(rectangle1.x, width/2,width);
 fill(rectangle1.fill, rectangle1.alpha)
 rectMode(CENTER);
 rect(rectangle1.x, rectangle1.y,rectangle1.width, rectangle1.height)
+
+//square with mouse
+fill(square1.fill, square1.alpha)
+square(mouseX,mouseY,square1.size)
+
 
 
 
