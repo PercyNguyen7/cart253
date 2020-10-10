9371bbc26910d17397251c1d7ebd57fe4f61746b
PercyNguyen7 <70938896+PercyNguyen7@users.noreply.github.com>
2020-10-09 20:35:06 -0400

    P1: Set up canvas, added first state, title to the stimulation

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,20 +1,36 @@
-/**************************************************
-Template p5 project
-Pippin Barr
-
-Here is a description of this template p5 project.
-**************************************************/
 
 // setup()
 //
 // Description of setup() goes here.
+let state = `title`;
+
 function setup() {
-background (255,0,0)
+  createCanvas(1000,600);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0)
 
+  if (state === `title`){
+        title();
+  }
+
+
+
+
+
+}
+
+function title(){
+  push();
+  textSize(38)
+  fill(200,100,100);
+  textAlign(CENTER,CENTER);
+  text(`Propose Day`,width/2,height/2)
+  textSize(30)
+  text(`Press any key...`,width/2,height/2 +100)
+  pop();
 }
