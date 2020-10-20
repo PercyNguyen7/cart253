d48215012c43630cd551c6eecb171e16f7842177
PercyNguyen7 <70938896+PercyNguyen7@users.noreply.github.com>
2020-10-20 14:31:41 -0400

    E4: Changed background color, added in ground and removed foods

--- exercises/exercise4/js/script.js
+++ exercises/exercise4/js/script.js
@@ -4,60 +4,27 @@
 let user = {
   x: 0,
   y: 0,
-  size: 100,
+  size: 50,
 };
 
 // Food objects
-let food1;
-let food2;
-let food3;
-let food4;
-let food5;
-let food6;
+let star;
 
 function setup() {
-  createCanvas(600, 600);
-
-  food1 = createFood(50, 300);
-  food2 = createFood(150, 300);
-  food3 = createFood(250, 300);
-  food4 = createFood(350, 300);
-  food5 = createFood(450, 300);
-  food6 = createFood(550, 300);
-}
-
-function createFood(x, y) {
-  let food = {
-    x: x,
-    y: y,
-    size: 50,
-    eaten: false,
-  };
-  return food;
+  createCanvas(windowWidth, 750);
 }
 
 function draw() {
-  background(0);
+  background(8, 43, 99);
 
   // Move the user (with the mouse)
   moveUser();
 
   // Check whether the user has eaten either food
-  checkFood(food1);
-  checkFood(food2);
-  checkFood(food3);
-  checkFood(food4);
-  checkFood(food5);
-  checkFood(food6);
 
   // Display the user and foods
   displayUser();
-  displayFood(food1);
-  displayFood(food2);
-  displayFood(food3);
-  displayFood(food4);
-  displayFood(food5);
-  displayFood(food6);
+  displayGround();
 }
 
 // Sets the user position to the mouse position
@@ -66,31 +33,19 @@ function moveUser() {
   user.y = mouseY;
 }
 
-// Checks if the user overlaps the food object and eats it if so
-function checkFood(food) {
-  if (!food.eaten) {
-    let d = dist(user.x, user.y, food.x, food.y);
-    if (d < user.size / 2 + food.size / 2) {
-      food.eaten = true;
-    }
-  }
-}
 // Draw the user as a circle
 function displayUser() {
   push();
-  fill(255);
+  stroke(253, 232, 85);
+  strokeWeight(3);
+  noFill();
   ellipse(user.x, user.y, user.size);
   pop();
 }
 
-// Draw the food as a circle
-function displayFood(food) {
-  // Check if the food is still available to be eaten
-  if (!food.eaten) {
-    // Display the food as its position and with its size
-    push();
-    fill(255, 100, 100);
-    ellipse(food.x, food.y, food.size);
-    pop();
-  }
+function displayGround() {
+  push();
+  fill(9, 24, 20);
+
+  rect(0, 600, width, 150);
 }
