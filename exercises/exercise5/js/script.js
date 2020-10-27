752a6f37bfe2dfa3c47cb49b9e17ada4bda1d8b2
PercyNguyen7 <70938896+PercyNguyen7@users.noreply.github.com>
2020-10-27 15:58:20 -0400

    E5: Added in title and gameplay states

--- exercises/exercise5/js/script.js
+++ exercises/exercise5/js/script.js
@@ -6,10 +6,12 @@ let paddle;
 let paddle2;
 
 let balls = [];
-let numBalls = 10;
+let numBalls = 100;
 
 let balls2 = []
-let numBalls2 = 10;
+let numBalls2 = 100;
+
+let state = `title`;
 
 function setup() {
   createCanvas(1500, 700);
@@ -19,7 +21,7 @@ function setup() {
 
   for (let i = 0; i < numBalls; i++) {
     let x = random(width/2 + 50, width);
-    let y = random(-400, -100);
+    let y = random(-1300, -900);
     let ball = new Ball(x, y);
     balls.push(ball);
   }
@@ -33,8 +35,37 @@ function setup() {
 }
 
 function draw() {
-  background(0);
+    background(0);
+  if (state === `title`) {
+    title();
+  } else if (state === `gameplay`) {
+    gameplay();
+  }
+  else if (state === `winending`) {
+    winending();
+  } else if (state === `loseending`) {
+    loseending();
+  }
+}
+//                                     STATE FUNCTIONS
+function title() {
+  push();
+  textSize(64)
+  noFill();
+  stroke(255);
+  textAlign(CENTER, CENTER);
+  textSize(80);
+  text(`Juggle or Struggle`, width / 2, height/2-50);
+  textSize(40);
+  text(`Save 3 balls. Last 12 seconds`, width/2, height/2 +50)
+  textSize(40);
+  text(`Controls`, width/2, height-100);
+  text(`A and D`, width/3, height - 50);
+  text(`Mouse`, 2*width/3, height -50);
+  pop();
+  }
 
+function gameplay(){
   paddle.move();
   paddle.display();
 
@@ -58,5 +89,11 @@ function draw() {
       ball2.bounce(paddle,paddle2);
       ball2.display();
     }
+  }
+}
+
+function mousePressed() {
+  if (state === `title`) {
+    state = `gameplay`;
   }
 }
