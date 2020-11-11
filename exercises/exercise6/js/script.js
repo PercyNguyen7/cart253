d3706e547d6d945bf52cb4b54de23c4090c0f4a1
PercyNguyen7 <70938896+PercyNguyen7@users.noreply.github.com>
2020-11-11 01:32:09 -0500

    E6: Using codes from Audio In class notes

--- exercises/exercise6/js/script.js
+++ exercises/exercise6/js/script.js
@@ -1,20 +1,31 @@
-/**************************************************
-Template p5 project
-Pippin Barr
+"use strict";
 
-Here is a description of this template p5 project.
-**************************************************/
+// The microphone
+let mic;
 
-// setup()
-//
-// Description of setup() goes here.
 function setup() {
-background (255,0,0)
+  createCanvas(600, 600);
+
+  // Create our AudioIn object
+  mic = new p5.AudioIn();
+  // Try to connect to the user's microphone
+  mic.start();
 }
 
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
+  background(0);
 
+  // Get the current level of sound going into the microphone
+  let level = mic.getLevel();
+
+  // Visualize the level as a circle's size
+  let size = map(level, 0, 1, 0, width);
+  push();
+  fill(255, 0, 0);
+  noStroke();
+  ellipse(width / 2, height / 2, size);
+  pop();
+
+
+  console.log(level)
 }
