072510d5454c24896a03c20adc6324438a95a9e4
PercyNguyen7 <70938896+PercyNguyen7@users.noreply.github.com>
2020-11-11 00:50:48 -0500

    Activity: finished sound activity

--- activities/sound-activity/js/script.js
+++ activities/sound-activity/js/script.js
@@ -1,20 +1,36 @@
-/**************************************************
-Template p5 project
-Pippin Barr
+"use strict";
+// Balls
+let balls = [];
 
-Here is a description of this template p5 project.
-**************************************************/
 
-// setup()
-//
-// Description of setup() goes here.
-function setup() {
-background (255,0,0)
+// F-minor
+let notes = [`F3`,`G3`, `Ab4`, `Bb4`, `C4`, `Db4`,`Eb4`,`F4`];
+
+function setup(){
+  createCanvas(600,600);
+
+  userStartAudio();
 }
 
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
+function draw(){
+  background(0);
 
+  for (let i = 0; i < balls.length; i++){
+      let ball = balls[i];
+      ball.move();
+      ball.bounce();
+      ball.display();
+
+  }
+}
+
+function mousePressed(){
+  createBall(mouseX,mouseY);
+}
+
+
+function createBall(x,y){
+  let note = random(notes);
+  let ball = new Ball(x,y,note);
+  balls.push(ball);
 }
