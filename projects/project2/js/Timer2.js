f71eccc67143031ef46f63fc5f58bc512bea73d3
PercyNguyen7 <70938896+PercyNguyen7@users.noreply.github.com>
2020-12-05 03:03:20 -0500

    P2: Added 3rd minigame where playere must figure out the exact sentence Yes I Will! as the magic word. Added 2 balls (timer 2) running down as timer. Also added new fonts

---
+++ projects/project2/js/Timer2.js
@@ -0,0 +1,50 @@
+class Timer2{
+  constructor(){
+    this.size = 50;
+    this.size2 = 50;
+
+    this.x = width/4 - 100;
+    this.y = 0-this.size/2;
+    this.vx = 0;
+    this.vy = 0.5;
+
+
+    this.fill= {
+      r : 255,
+      g : 255,
+      b : 255,
+    }
+
+    this.x2 = 3*width/4 + 100;
+    this.y2 = 0-this.size2/2;
+    this.vx2 = 0;
+    this.vy2 = 0.5;
+
+  }
+touchbottom(){
+  if (this.y - this.size/2 > height){
+    state = `heleftEnd`;
+    acceptmusic.stop();
+    loseSFX.play();
+  }
+}
+
+move(){
+  this.y += this.vy;
+  this.y2 += this.vy2;
+}
+
+display(){
+  push();
+  noStroke();
+  let r = map(this.y,0,height,0,220);
+  let g = map(this.y,0,height,0,30);
+  let b = map(this.y,0,height,0,30);
+  fill(r,g,b);
+  ellipse(this.x, this.y, this.size);
+  ellipse(this.x2, this.y2, this.size2);
+  pop();
+}
+
+
+}
--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -1,10 +1,17 @@
 "use strict";
 //                                           VARIABLES
+//FONTS
+let onyxFont;
+let tradegothicFont;
+
+let magicWord = `yes i will!`
+let riddleText = `Will you marry me?`
+let currentInput = ``;
 let visible = true;
 let fillbg = {
-  r:176,
-  g:31,
-  b:44,
+  r: 176,
+  g: 31,
+  b: 44,
 }
 let userreject;
 let userHeart;
@@ -12,7 +19,8 @@ let userHeart2;
 let timer1;
 let chain1;
 let memos = [];
-let numMemos = 1;
+let numMemos = 0;
+let timer2;
 
 //                                            SFX/ MUSIC
 let menumusic;
@@ -40,17 +48,21 @@ let numTiles = 100;
 let state = `clickbegin`;
 //                                                IMAGE PRELOAD
 function preload() {
-  // SOUND
-  menumusic = loadSound(`assets/sounds/menu.mp3`)
-  acceptmusic = loadSound(`assets/sounds/POSING.mp3`);
-  rejectmusic = loadSound(`assets/sounds/HTRJ.mp3`)
+  //FONTS
+  onyxFont = loadFont(`assets/fonts/onyx.ttf`);
+  tradegothicFont = loadFont(`assets/fonts/Trade Gothic LT Condensed No. 18.ttf`);
 
-  winSFX = loadSound(`assets/sounds/winSFX.mp3`)
-  loseSFX= loadSound(`assets/sounds/loseSFX.mp3`)
+  // SOUND
+  menumusic = loadSound(`assets/sounds/menu.mp3`);
+  acceptmusic = loadSound(`assets/sounds/POSING.mp3`);
+  rejectmusic = loadSound(`assets/sounds/HTRJ.mp3`);
+
+  winSFX = loadSound(`assets/sounds/winSFX.mp3`);
+  loseSFX = loadSound(`assets/sounds/loseSFX.mp3`);
   upSFX = loadSound(`assets/sounds/go_up.m4a`);
   downSFX = loadSound(`assets/sounds/head_down.m4a`);
   rightSFX = loadSound(`assets/sounds/go_right.m4a`);
-  leftSFX = loadSound(`assets/sounds/go_left.m4a`)
+  leftSFX = loadSound(`assets/sounds/go_left.m4a`);
   // IMAGE
   userrejectImage = loadImage(`assets/images/userreject.png`);
   userHeartImage = loadImage(`assets/images/heart.png`);
@@ -60,20 +72,21 @@ function preload() {
 //                                                     SETUP
 function setup() {
   createCanvas(1500, 700);
-  textAlign(CENTER,CENTER);
-//                                                  AUDIO
+  textAlign(CENTER, CENTER);
+  textFont(tradegothicFont);
+  //                                                  AUDIO
   userStartAudio()
-   oscillator = new p5.Oscillator(880, `sine`);
-   winSFX.amp(0.4);
-   rightSFX.amp(2.5);
-   leftSFX.amp(3.5);
-   downSFX.amp(2.5);
-   upSFX.amp(2.5);
-   oscillator.amp(0.3);
+  oscillator = new p5.Oscillator(880, `sine`);
+  winSFX.amp(0.4);
+  rightSFX.amp(2.5);
+  leftSFX.amp(3.5);
+  downSFX.amp(2.5);
+  upSFX.amp(2.5);
+  oscillator.amp(0.3);
 
-   menumusic.amp(0.4);
-   acceptmusic.amp(0.4);
-   rejectmusic.amp(0.25);
+  menumusic.amp(0.4);
+  acceptmusic.amp(0.4);
+  rejectmusic.amp(0.25);
   //                                                 ACCEPT PATH
   userHeart2 = new UserHeart2(userHeartImage);
 
@@ -89,9 +102,11 @@ function setup() {
     let memo = new Memo(x, y);
     memos.push(memo);
 
-  // BLINK AT minigame 2 accept route
+    // BLINK AT minigame 2 accept route
     setInterval(blink, 1000);
   }
+
+  timer2 = new Timer2();
   //                                                 REJECTION PATH
   userreject = new UserReject(userrejectImage);
   //                                                  PURPLE? TILES
@@ -100,7 +115,7 @@ function setup() {
     let x = 200;
     let y = -100;
     for (let j = 0; j < 7; j++) {
-      y +=  100
+      y += 100
       let tile = new Tile(x, y, color);
       tiles.push(tile);
     }
@@ -213,7 +228,6 @@ function setup() {
       tiles.push(tile);
     }
   }
-
 }
 //                                                DRAW
 function draw() {
@@ -243,26 +257,35 @@ function draw() {
   if (state === `acpminigame1`) {
     acpminigame1();
   }
-  if (state === `heartattackEnd`){
+  if (state === `heartattackEnd`) {
     heartattackEnd();
   }
-  if (state === `acpminiwin1`){
+  if (state === `acpminiwin1`) {
     acpminiwin1();
   }
-  if (state === `acpintro2`){
+  if (state === `acpintro2`) {
     acpintro2();
   }
-  if (state === `acpminigame2`){
+  if (state === `acpminigame2`) {
     acpminigame2();
   }
-  if (state === `panicEnd`){
+  if (state === `panicEnd`) {
     panicEnd();
   }
-  if (state === `acpminiwin2`){
+  if (state === `acpminiwin2`) {
     acpminiwin2();
   }
-  if (state === `acpintro3`){
+  if (state === `acpintro3`) {
     acpintro3();
+  }
+  if (state === `acpminigame3`) {
+    acpminigame3();
+  }
+  if (state === `acpsuccess`) {
+    acpsuccess();
+  }
+  if (state === `heleftEnd`) {
+    heleftEnd();
   }
 
   //                                          REJECT PATH
@@ -281,105 +304,126 @@ function draw() {
   if (state === `goodrejectionEnd`) {
     goodrejectionEnd();
   }
-
 }
 //                                                  GAME INTRO
 function clickbegin() {
   background(0);
   push();
-  textSize(64)
-  noFill();
+  noFill(0);
+  textSize(50);
+  strokeWeight(1);
   stroke(255);
-  textSize(80);
-  text(`Press Spacebar to begin`, width / 2, height / 2);
+  text(`Press Spacebar to navigate through the game`, width / 2, height / 2);
   pop();
 }
 //                                                     TITLE
 function title() {
-  background(50);
+  background(36,69,133);
   push();
-  textSize(64)
-  noFill();
-  stroke(255);
-  textSize(80);
-  text(`Proposal Day`, width / 2, height / 2);
+  textFont(onyxFont);
+  strokeWeight(3);
+  stroke(0);
+  fill(255);
+  textSize(120);
+  text(`Proposal Day 2`, width / 2, height / 2);
   pop();
 }
 //                                                    INTRODUCTION
 function instructions() {
-  background(100);
+  background(167,1,7);
+
   push();
-  textSize(64)
-  noFill();
-  stroke(255);
-  textSize(80);
-  text(`Instructions`, width / 2, height / 2);
-  textSize(40);
-  text(`Tap any key to advance throughout the game`, width / 2, height / 2 + 100);
+  strokeWeight(3);
+  stroke(0);
+  fill(255);
+  textSize(120);
+  textFont(onyxFont);
+  text(`Instructions`, width / 2, height/2 - 100);
+  textFont(tradegothicFont);
+  textSize(60);
+  text(`Control with Keyboard`, width / 2, height / 2 +50);
+  textSize(50);
+  text(`Stay on this slide to enjoy the lovely background lounge jazz`, width/2, height / 2 + 120)
   pop();
 }
 //                                                     CONTEXT
 function context() {
   background(150);
   push();
-  textSize(64);
-  noFill();
-  stroke(255);
-  textSize(100);
-  text(`context`, width / 2, height / 2);
+  fill(255);
+  textSize(50);
+  text(`You watch as the man of your life kneels down`, width / 2, 50);
+  text(`and propose...`, width / 2, 100);
   pop();
 }
 //                                                      DECISION
 function decision() {
   background(0);
   push();
-  textSize(64);
-  noFill();
   stroke(255);
+  noFill();
+  strokeWeight(2);
   textSize(100);
-  text(`Decision`, width / 2, height / 2);
-  textSize(40);
-  text(`A. Accept`, width / 3, 3 * height / 4);
-  text(`R. Reject`, 2 * width / 3, 3 * height / 4);
+  text(`So what is it going to be, chief?`, width / 2, height / 2 - 100);
+  textFont(tradegothicFont);
+  textSize(120);
+  stroke(180,1,7);
+  noFill();
+  text(`A. Accept!`, width / 3,  2*height / 3);
+  stroke(36,69,150);
+  // fill(36,69,150);
+  textSize(50);
+  text(`R. Reject...`, 2 * width / 3, 2* height / 3);
   pop();
 }
 //                                                   ACCEPT PATH
 function acceptp() {
-  background(0, 0, 255);
+  background(36,69,133);
   push();
+  strokeWeight(2);
+  stroke(0);
   textSize(64);
-  noFill();
-  stroke(255);
+  fill(255,223,85);
+  textSize(50);
+  text(`...but of course`, width/2, height/ 2 -150);
   textSize(100);
-  text(`ACCEPTED`, width / 2, height / 2);
+  text(`BUT OF COURSEEEEE!!!!`, width / 2, height / 2 - 50);
+  textSize(50);
+  text(`...you thought inside your head`, width / 2, height / 2 + 100);
+  textSize(30);
+  text(`But the words won't come out...`, width / 2, height / 2 + 170);
+  textSize(50);
+  text(`You feel your heart beats faster by the second...`, width / 2, height / 2 + 240);
   pop();
 }
-function acpintro1(){
-  background(69, 29, 87);
+
+function acpintro1() {
+  background(167,1,7);
   push();
-  textSize(64)
-  noFill();
-  stroke(250, 186, 95);
-  textSize(80);
-  text(`Contain your heart`, width / 2, height / 2 - 100);
+  stroke(0);
+  strokeWeight(3);
+  fill(255);
+  textSize(120);
+  textFont(onyxFont);
+  text(`Contain your heart`, width / 2, height / 2 - 120);
   textSize(70);
   text(`Don't break the chains`, width / 2, height / 2 - 20);
+  textFont(tradegothicFont);
   textSize(50);
   text(`Press Spacebar to BEGIN`, width / 2, height / 2 + 100);
-  textSize(40);
+  textSize(50);
   text(`Control with Arrow Keys`, width / 2, height / 2 + 150);
   pop();
 }
 
-function acpminigame1(){
-  background(160,1,7);
+function acpminigame1() {
+  background(160, 1, 7);
   //                                         TIMER
   timer1.blackout();
   timer1.ballsmove();
   timer1.display();
 
-
-//                                        USER HEART
+  //                                        USER HEART
   userHeart.display();
   userHeart.move();
   userHeart.touchchain();
@@ -388,141 +432,196 @@ function acpminigame1(){
   chain1.display();
 }
 //                                                     HEART ATTACK ENDING
-function heartattackEnd(){
+function heartattackEnd() {
   background(0);
   push();
-  noFill();
-  stroke(255);
+  stroke(0);
+  strokeWeight(1);
+  fill(255);
   textSize(50);
-  text(`How do you mend a heart attack`, width/2, height/2);
+  text(`How do you mend a heart attack`, width / 2, height / 2);
   textSize(70);
   pop();
 }
 
-function acpminiwin1(){
-  background(200,2,10);
+function acpminiwin1() {
+  background(36,69,133);
   push();
-  noFill();
-  stroke(255);
-  textSize(50);
-  text(`You survived the first trial of love!`, width/2, height/2);
-  text(`Now face the 2nd :)`, width/2, height/2 +100);
+  stroke(0);
+  strokeWeight(2);
+  fill(255);
+  textSize(60);
+  text(`You contained your emotions and survived the first trial of love!`, width / 2, height/2 - 50);
+  textSize(100);
+  text(`NOW FACE THE SECOND`, width / 2, height / 2 + 50);
   pop();
 }
 
-function acpintro2(){
+function acpintro2() {
   background(100);
   push();
-  noFill();
-  stroke(255);
+  fill(255);
   textSize(50);
-  text(`Collect your memories`, width/2, height/2);
+  textFont(onyxFont);
+  text(`Collect your memories`, width / 2, height / 2 - 120);
+  textSize(70);
+  text(`Don't break the chains`, width / 2, height / 2 - 20);
+  textFont(tradegothicFont);
+  text(`Control with Arrow Keys`, width / 2, height / 2);
+  text(``, width / 2, height / 2);
   pop();
 }
 
-function acpminigame2(){
-  background(fillbg.r,fillbg.g,fillbg.b);
+function acpminigame2() {
+  background(fillbg.r, fillbg.g, fillbg.b);
 
   blinkog();
 
-//  memos
+  //  memos
   for (let i = 0; i < memos.length; i++) {
-      let memo = memos[i];
+    let memo = memos[i];
 
-      if (memo.active) {
-        if (memo.tstop === true){
-          fillbg ={
-            r:34,
-            g:64,
-            b:123
-          }
+    if (memo.active) {
+      if (memo.tstop === true) {
+        fillbg = {
+          r: 34,
+          g: 64,
+          b: 123
         }
-        else {
-          fillbg ={
-            r:152,
-            g:35,
-            b:43,
-          }
+      } else {
+        fillbg = {
+          r: 152,
+          g: 35,
+          b: 43,
         }
-    memo.move();
-    memo.bounce();
-    memo.display();
-    let d = dist(memo.x, memo.y, userHeart2.x, userHeart2.y);
-      if (d < memo.size/2 + userHeart2.size/2){
-         memos.splice(i, 1);
-         break;
-       }
-     }
-   }
+      }
+      memo.move();
+      memo.bounce();
+      memo.display();
+      let d = dist(memo.x, memo.y, userHeart2.x, userHeart2.y);
+      if (d < memo.size / 2 + userHeart2.size / 2) {
+        memos.splice(i, 1);
+        break;
+      }
+    }
+  }
 
   //                                                  WIN GAME CONDITION
-    if (memos.length <= 0){
+  if (memos.length <= 0) {
     state = `acpminiwin2`
     winSFX.play();
   }
-//                                               HEART AND PURSUER
-    userHeart2.display();
-    userHeart2.move();
-    userHeart2.displaypursuit();
-    userHeart2.movepursuit1();
-    userHeart2.caught();
+  //                                               HEART AND PURSUER
+  userHeart2.display();
+  userHeart2.move();
+  userHeart2.displaypursuit();
+  userHeart2.movepursuit1();
+  userHeart2.caught();
 }
 
 //                          BLINK FUNCTIONS
-function blinkog(){
+function blinkog() {
   push();
 
-  if (visible && userHeart2.size === 30){
+  if (visible && userHeart2.size === 30) {
     fill(0);
     rectMode(CENTER);
-    rect(width/2 - 150,height/2,200, 500);
-    rect(width/2 + 150,height/2,200, 500);
+    rect(width / 2 - 150, height / 2, 200, 500);
+    rect(width / 2 + 150, height / 2, 200, 500);
   }
   pop();
 }
-function blink(){
+
+function blink() {
   visible = !visible;
 }
 
-function panicEnd(){
+function panicEnd() {
+  background(0);
+  push();;
+  fill(255);
+  textSize(50);
+  text(`Panic Attac`, width / 2, height / 2);
+  pop();
+
+}
+
+function acpminiwin2() {
+  background(200);
+  push();;
+  fill(255);
+  textSize(50);
+  text(`You gathered your thoughts`, width / 2, height / 2);
+  pop();
+}
+
+function acpintro3() {
+  background(250);
+  push();;
+  fill(255);
+  textSize(50);
+  text(`Final Challenge`, width / 2, height / 2);
+  pop();
+}
+
+function acpminigame3() {
+  // Check if they've got it right yet
+  let correct = checkInput();
+  background(0);
+  if (correct) {
+    state = `acpsuccess`;
+  }
+
+  push();;
+  fill(255);
+  textSize(50);
+  // Display the riddle
+  text(riddleText, width / 2, height / 3);
+  // Display the current input from the user
+  text(currentInput, width / 2, 2 * height / 3);
+  pop();
+
+  timer2.display();
+  timer2.move();
+  timer2.touchbottom();
+}
+
+function checkInput() {
+  // First convert the input to lower case so we can
+  // ignore any capitalization stuff
+  let lowerCaseInput = currentInput.toLowerCase();
+  // Check if the converted input is the same as the magic word
+  if (lowerCaseInput === magicWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function heleftEnd() {
   background(0);
   push();
-  noFill();
-  stroke(255);
-  textSize(50);
-  text(`Panic Attac`, width/2, height/2);
+  textSize(64);;
+  fill(255);
+  textSize(100);
+  text(`He left...`, width / 2, height / 2);
   pop();
-
 }
-function acpminiwin2(){
-  background(200);
+
+function acpsuccess() {
+  background(255, 0, 0);
   push();
-  noFill();
-  stroke(255);
-  textSize(50);
-  text(`You gathered your thoughts`, width/2, height/2);
+  textSize(64);;
+  fill(255);
+  textSize(100);
+  text(`YOU DID IT!!!`, width / 2, height / 2);
   pop();
 }
-
-function acpintro3(){
-  background(250);
-  push();
-  noFill();
-  stroke(255);
-  textSize(50);
-  text(`Final Challenge`, width/2, height/2);
-  pop();
-}
-
-
-
 //                                                    REJECTION PATH
 function rejectp() {
   background(255, 0, 0);
   push();
   textSize(64);
-  noFill();
-  stroke(255);
   textSize(100);
   text(`REJECTED`, width / 2, height / 2);
   pop();
@@ -532,8 +631,7 @@ function rejectionintro() {
   background(69, 29, 87);
   push();
   textSize(64)
-  noFill();
-  stroke(250, 186, 95);
+  fill(250, 186, 95);
   textSize(80);
   text(`Go through`, width / 2, height / 2 - 100);
   textSize(70);
@@ -562,15 +660,13 @@ function rejminigame() {
   //                                                DESTINATION RECTANGLES
   rect(1300, 0, 100, 700);
 }
-
 //                                                              TWO REJECTIOn ENDING STATES
 //                                                               BAD REJECTION ENDING
 function badrejectionEnd() {
   background(255, 40, 40);
   push();
   textSize(64)
-  noFill();
-  stroke(255);
+  fill(255);
   textSize(80);
   text(`He never found love again`, width / 2, height / 2);
   pop();
@@ -580,8 +676,7 @@ function goodrejectionEnd() {
   background(125);
   push();
   textSize(64)
-  noFill();
-  stroke(255);
+  fill(255);
   textSize(70);
   text(`A painful truth`, width / 2, height / 2);
   textSize(90);
@@ -592,57 +687,69 @@ function goodrejectionEnd() {
 
 //                                                     KEYPRESSED FUNCTION
 function keyPressed() {
-  if (state === `clickbegin`&& keyCode === 32) {
+  if (state === `clickbegin` && keyCode === 32) {
     state = `title`;
     menumusic.loop();
-  } else if (state === `title`&& keyCode === 32) {
+  } else if (state === `title` && keyCode === 32) {
     state = `instructions`;
-  } else if (state === `instructions`&& keyCode === 32) {
+  } else if (state === `instructions` && keyCode === 32) {
     state = `context`;
     menumusic.amp(0.05);
-  } else if (state === `context`&& keyCode === 32) {
+  } else if (state === `context` && keyCode === 32) {
     state = `decision`;
-    if (menumusic.isPlaying()){
-     menumusic.stop();
-   }
+    if (menumusic.isPlaying()) {
+      menumusic.stop();
+    }
   }
-//                                                ACCEPT PATH
+  //                                                ACCEPT PATH
   else if (state === `decision` && keyCode === 65) {
     state = `acceptp`;
     acceptmusic.loop();
-  } else if (state === `acceptp`&& keyCode === 32) {
+  } else if (state === `acceptp` && keyCode === 32) {
     state = `acpintro1`;
-  } else if (state ===`acpintro1`&& keyCode === 32) {
+  } else if (state === `acpintro1` && keyCode === 32) {
     state = `acpminigame1`
   } else if (state === `acpminiwin1` && keyCode === 32) {
     state = `acpintro2`;
   } else if (state === `acpintro2` && keyCode === 32) {
     state = `acpminigame2`;
   }
-// TURN TSTOP TO TRUE IF A key is PRESSED
+  // TURN TSTOP TO TRUE IF A key is PRESSED
   else if (state === `acpminigame2` && keyCode === 65) {
-      for (let i = 0; i < memos.length; i++) {
-          let memo = memos[i];
-          memo.tstop = true;
-      }
-  // TURN TSTOP MODE TO FALSE IF S KEY IS PRESSED
+    for (let i = 0; i < memos.length; i++) {
+      let memo = memos[i];
+      memo.tstop = true;
+    }
+    // TURN TSTOP MODE TO FALSE IF S KEY IS PRESSED
   } else if (state === `acpminigame2` && keyCode === 83 &&
     userHeart2.size2 === 450) {
-        for (let i = 0; i < memos.length; i++) {
-            let memo = memos[i];
-            memo.tstop = false;
-        }
-  }  else if (state === `acpminiwin2`&& keyCode === 32){
-        state = `acpintro3`;
+    for (let i = 0; i < memos.length; i++) {
+      let memo = memos[i];
+      memo.tstop = false;
+    }
+  } else if (state === `acpminiwin2` && keyCode === 32) {
+    state = `acpintro3`;
+  } else if (state === `acpminiwin2` && keyCode === 32) {
+    state = `acpintro3`;
+  } else if (state === `acpintro3` && keyCode === BACKSPACE) {
+    state = `acpminigame3`;
+  } else if (state === `acpminigame3` && keyCode === 8) {
+    currentInput = ``;
   }
 
-//                                                 REJECT PATH
+  //                                       REJECT PATH
   else if (state === `decision` && keyCode === 82) {
     state = `rejectp`;
     rejectmusic.loop();
   } else if (state === `rejectp`) {
     state = `rejectionintro`;
   } else if (state === `rejectionintro` && keyCode === 32) {
-      state = `rejminigame`;
+    state = `rejminigame`;
+  }
+}
+
+function keyTyped() {
+  if (state === `acpminigame3`) {
+    currentInput += key;
   }
 }
